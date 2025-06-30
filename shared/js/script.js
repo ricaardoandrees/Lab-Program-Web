44997fc7ac277a7d2967c9801daffe4fbb7b637f
Ricardo <ricaardoandresma@gmail.com>
2025-06-29 20:09:40 -0400

    FINAL VERSION

--- shared/js/script.js
+++ shared/js/script.js
@@ -34,25 +34,4 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('errorAlert').style.display = 'block';
         }
     });
-
-    // Código para el formulario de cambiar clave
-    document.querySelector('.config-form[action="/cambiar-clave"]').addEventListener('submit', function(event) {
-        event.preventDefault();
-
-       
-        const usuario = document.getElementById('usuario').value;
-        const oldPassword = document.getElementById('old-password').value;
-        const newPassword = document.getElementById('new-password').value;
-
-        
-        localStorage.setItem('usuario', usuario);
-        localStorage.setItem('oldPassword', oldPassword);
-        localStorage.setItem('newPassword', newPassword);
-
-        
-        alert('¡Cambio de clave exitoso!');
-
-       
-        this.reset();
-    });
 });
